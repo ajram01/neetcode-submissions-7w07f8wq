86bbff7ce0fbfcf7a46c15688293c66fca52b02d
Anthony Ramirez <104178473+ajram01@users.noreply.github.com>
2026-04-28 14:58:28 -0500

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(0, nums, target, 0, new ArrayList<>(), result);
+        return result;
+        
+    }
+
+    public void backtrack(int start, int[] nums, int target, int currSum, List<Integer> curr, List<List<Integer>> result){
+
+        if (currSum == target){
+            result.add(new ArrayList<>(curr));
+            return;
+        }
+
+        if (currSum > target) return;
+
+        for (int i = start; i < nums.length; i++){
+
+            curr.add(nums[i]);
+
+            backtrack(i, nums, target, currSum + nums[i], curr, result);
+
+            curr.remove(curr.size() - 1);
+        }
+    }
+}
