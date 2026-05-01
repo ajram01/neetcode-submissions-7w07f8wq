2474fdc1c19b6d6a3db6b7c8d537ace9b35f96a8
Anthony Ramirez <104178473+ajram01@users.noreply.github.com>
2026-05-01 11:34:19 -0500

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.java
@@ -0,0 +1,50 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+
+        if (node == null) return null;
+        
+        HashMap<Node, Node> cloned = new HashMap<>();
+        return dfs(node, cloned);
+    
+    }
+
+    public Node dfs(Node currNode, HashMap<Node, Node> cloned){
+
+        if (cloned.containsKey(currNode)) return cloned.get(currNode);
+
+        Node clonedNode = new Node();
+        clonedNode.val = currNode.val;
+
+        cloned.put(currNode, clonedNode);
+
+        for (int i = 0; i < currNode.neighbors.size(); i++){
+            
+            Node neighbor = currNode.neighbors.get(i);
+            Node clonedNeighbor = dfs(neighbor, cloned);
+            clonedNode.neighbors.add(clonedNeighbor);
+        }
+
+        return clonedNode;
+
+    }
+}
